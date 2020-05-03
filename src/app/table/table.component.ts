b9dbfc48954104271247c51375b634eb82a9076b
tarunchawla28 <tctaruntc@gmail.com>
2020-05-04 01:32:56 +0530

    entering cuisine

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -12,9 +12,6 @@ import { MatSort } from '@angular/material/sort';
 export class TableComponent implements OnInit {
   ngOnInit(): void {
     this.dataSource.sort = this.sort;
-    this.dataSource.filterPredicate = function (data, filter: string): boolean {
-      return data.Cuisines.toLowerCase().includes(filter);
-    }
   }
 
   @ViewChild(MatSort, { static: true }) sort: MatSort;
@@ -25,13 +22,38 @@ export class TableComponent implements OnInit {
   dataSource = new MatTableDataSource(RESTAURANT_DATA);
 
   applyFilterCuisine(event: Event) {
-    console.log('hello');
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    const filterValueString = (event.target as HTMLInputElement).value;
+    // var array = filterValue.split(',');
+    // console.log(array);
+    // array.forEach(filterValue => {
+    //   this.dataSource.filter = filterValue.trim().toLowerCase()
+    // })
+    this.dataSource.filterPredicate = function (data, filter: string): boolean {
+      const matchFilter = [];
+      console.log('data', data)
+      var filterArray = filter.split(',');
+      console.log('FlterArray');
+      console.log(filterArray)
+      filterArray.forEach(filtervalue => {
+        console.log('filter value ' + filtervalue);
+        if (data.Cuisines.toLowerCase().includes(filtervalue)) {
+          matchFilter.push(true);
+        }
+        if (!data.Cuisines.toLowerCase().includes(filtervalue)) {
+          matchFilter.push(false);
+        }
+      })
+      console.log(matchFilter)
+      return matchFilter.some(Boolean)
+    }
+    this.dataSource.filter = filterValueString.trim().toLowerCase();
   }
   applyFilterName(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.dataSource.filterPredicate = function (data, filter: string): boolean {
+      return data.Restaurant_Name.toLowerCase().includes(filter);
+    }
   }
 }
 
